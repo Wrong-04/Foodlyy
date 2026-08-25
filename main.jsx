09ef7f3c4e562b4a92db171e860a6336b27f7ef3
Huy04@ <he182181nguyenquanghuy@gmail.com>
2026-08-25 14:54:37 +0700

    fix

--- main.jsx
+++ main.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { loadDB } from './lib/db';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error('Could not find root element to mount to');
-}
+const init = async () => {
+  await loadDB();
+  const rootElement = document.getElementById('root');
+  if (!rootElement) {
+    throw new Error('Could not find root element to mount to');
+  }
 
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+init();
